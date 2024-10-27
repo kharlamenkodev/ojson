80e2778eb28237dcf56bbd006bb6e78c5ec365ba
i.kharlamenko <i.kharlamenko@1440.space>
2024-10-27 12:15:44 +0300

    Class for convertion node to double implemented

---
+++ include/node_as_double.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "node.hpp"
+
+#include <nlohmann/json.hpp>
+
+#include <vector>
+#include <string>
+#include <stdint.h>
+
+namespace ojson {
+
+class NodeAsDouble final : public Node
+{
+public:
+    explicit NodeAsDouble(const Node &node) : _origin{node}
+    {}
+
+    std::string content() const override { return _origin.content(); }
+    const std::vector<uint8_t>& bytes() const override { return _origin.bytes(); }
+    double value() const { return nlohmann::json::from_msgpack(_origin.bytes()).get<double>(); };
+
+private:
+    const Node& _origin;
+};
+
+} // namespace ojson
---
+++ tests/node_as_double_tests.cpp
@@ -0,0 +1,18 @@
+#include <gtest/gtest.h>
+
+#include "basic_node.hpp"
+#include "node_as_double.hpp"
+
+#include <nlohmann/json.hpp>
+
+TEST(node_as_double_tests, factor_from_basic_node_must_be_equal_0_243)
+{
+    ojson::BasicNode root_node({
+        {"salary", 200},
+        {"factor", 0.243}
+    });
+
+    double factor = ojson::NodeAsDouble(root_node.node("factor")).value();
+
+    EXPECT_EQ(factor, 0.243);
+}
