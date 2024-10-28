635f6c17ab259560c83dcc79e325579d742a95b5
i.kharlamenko <i.kharlamenko@1440.space>
2024-10-28 20:01:32 +0300

    Class for convertion node to array implemented

---
+++ include/node_as_array.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "node.hpp"
+#include "basic_node.hpp"
+
+#include <nlohmann/json.hpp>
+
+#include <vector>
+#include <string>
+#include <stdint.h>
+
+namespace ojson {
+
+class NodeAsArray final : public Node
+{
+public:
+
+    explicit NodeAsArray(const Node &node) : _origin{node}
+    {}
+
+    std::string content() const override { return _origin.content(); }
+    const std::vector<uint8_t>& bytes() const override { return _origin.bytes(); }
+
+    size_t size() const { return nlohmann::json::from_msgpack(_origin.bytes()).get<nlohmann::json::array_t>().size(); }
+
+    BasicNode operator[](size_t index) {
+        return BasicNode(nlohmann::json::from_msgpack(_origin.bytes()).get<nlohmann::json::array_t>().at(index));
+    }
+private:
+    const Node& _origin;
+};
+
+} // namespace ojson
---
+++ tests/node_as_array_tests.cpp
@@ -0,0 +1,36 @@
+#include <gtest/gtest.h>
+
+#include "basic_node.hpp"
+#include "node_as_array.hpp"
+#include "node_as_integer.hpp"
+
+#include <nlohmann/json.hpp>
+
+TEST(node_as_array_tests, array_from_basic_node_must_have_size_4)
+{
+    ojson::BasicNode root_node({
+        {"name", "Andrew"},
+        {"numbers", {1, 2, 3, 4}}
+    });
+
+    auto node = root_node.node("numbers");
+    auto as_array_node = ojson::NodeAsArray(node);
+
+    EXPECT_EQ(as_array_node.size(), 4);
+}
+
+TEST(node_as_array_tests, by_square_brackets_operator_accessed_elements_must_be_valid)
+{
+    ojson::BasicNode root_node({
+        {"name", "Andrew"},
+        {"numbers", {0, 1, 2, 3}}
+    });
+
+    auto node = root_node.node("numbers");
+    auto as_array_node = ojson::NodeAsArray(node);
+
+    for(size_t i = 0 ; i < as_array_node.size(); i++) {
+        int number = ojson::NodeAsInteger(as_array_node[i]).value();
+        EXPECT_EQ(number, i);
+    }
+}
